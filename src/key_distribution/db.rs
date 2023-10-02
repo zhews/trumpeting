936a9ef39841ff8e012661d9750d6ef8e0fb127c
Joel Graf <118117386+zhews@users.noreply.github.com>
2023-10-02 22:35:27 +0200

    feat: add postgres implementation of key distribution repository

--- src/key_distribution/db.rs
+++ src/key_distribution/db.rs
@@ -1,4 +1,5 @@
 use async_trait::async_trait;
+use deadpool_postgres::Pool;
 use std::error::Error;
 
 #[async_trait]
@@ -10,4 +11,41 @@ trait KeyDistributionRepository {
         public_key_signature: Vec<u8>,
     ) -> Result<(), Box<dyn Error>>;
     async fn get_public_key(&self, username: &str) -> Result<(Vec<u8>, Vec<u8>), Box<dyn Error>>;
+}
+
+struct PostgresKeyDistributionRepository {
+    connection_pool: Pool,
+}
+
+#[async_trait]
+impl KeyDistributionRepository for PostgresKeyDistributionRepository {
+    async fn store_public_key(
+        &self,
+        username: &str,
+        public_key: Vec<u8>,
+        public_key_signature: Vec<u8>,
+    ) -> Result<(), Box<dyn Error>> {
+        let client = self.connection_pool.get().await?;
+        let statement = client
+            .prepare_cached(
+                "INSERT INTO public_key (username, public_key, public_key_signature) VALUES ($1, $2, $3)"
+            )
+            .await?;
+        client
+            .execute(&statement, &[&username, &public_key, &public_key_signature])
+            .await?;
+        Ok(())
+    }
+    async fn get_public_key(&self, username: &str) -> Result<(Vec<u8>, Vec<u8>), Box<dyn Error>> {
+        let client = self.connection_pool.get().await?;
+        let statement = client
+            .prepare_cached(
+                "SELECT public_key, public_key_signature FROM public_key WHERE username = $1",
+            )
+            .await?;
+        let result = client.query_one(&statement, &[&username]).await?;
+        let public_key = result.try_get("public_key")?;
+        let public_key_signature = result.try_get("public_key_signature")?;
+        Ok((public_key, public_key_signature))
+    }
 }
